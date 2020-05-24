22d73f0dd01f1b312e313db614f05f022b02fefb
Ben <bmdvpanga@gmail.com>
2020-05-24 15:26:13 -0400

    Update mongodb connection string to use mongodb Atlas

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 
 
 //connect to mongoose db
-mongoose.connect("mongodb://localhost:27017/usersDB", {
+mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false
